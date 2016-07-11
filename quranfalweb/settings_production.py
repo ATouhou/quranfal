a807298cf12834756525c8757ad4e586799e4792
mehmet <mehmet@edgle.com>
2016-07-11 16:24:26 -0500

    added quranicjourney.com to the settings

--- quranfalweb/settings_production.py
+++ quranfalweb/settings_production.py
@@ -3,7 +3,7 @@
 from .settings_common import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quran4.me', 'www.quran4.me']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quran4.me', 'www.quran4.me', 'www.quranicjourney.com', 'quranicjourney.com']
 
 
 DATABASES['default'] = {
